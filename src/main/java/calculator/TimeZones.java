a75688decfe7a81fcc260b0d45932ccdaea9b2b5
Bossart Laurent <bossartlaurent@gmail.com>
2021-04-18 11:29:00 +0200

    Time Zone update

---
+++ src/main/java/calculator/TimeZones.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public enum TimeZones {
+    // TODO only use most common
+
+    public final String label;
+
+    private TimeZones(String label){
+        this.label = label;
+    }
+
+}
