dd3953bce3f5e923841d6c58aaf37822c5bc3b05
groundziro <171069@umons.ac.be>
2021-04-26 12:30:27 +0200

    Jar Functional

---
+++ src/main/java/calculator/Launcher.java
@@ -0,0 +1,10 @@
+package calculator;
+
+/**
+ * This class is used to launch the jar with the Shade plugin in Maven
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        GUI.Main.main(args);
+    }
+}
